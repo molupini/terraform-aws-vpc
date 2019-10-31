d0118ab22acd46f5720832710369ad90aab10eb4
molupini <info@myloft.co.za>
2019-10-31 16:49:19 +0200

    updated jenkins file and helper

--- bin/util/helper.py
+++ bin/util/helper.py
@@ -68,7 +68,48 @@ class HttpFetch(object):
         else:
             raise 1
 
-    def status(self, id, link='null'):
+    # def status(self, id, link='null'):
+    #         if link == 'null':
+    #             protocol = environ.get('IAC_ENDPOINT_PROTOCOL')
+    #             hostname = environ.get('IAC_ENDPOINT_HOSTNAME')
+    #             port = environ.get('IAC_ENDPOINT_PORT')
+    #             uri = f'{protocol}://{hostname}:{port}'
+    #         else:
+    #             uri = f'{link}'
+    #         url = f'/resources/query/{id}?document=tagging'
+    #         n = 0
+    #         d = 0
+    #         # debugging
+    #         # print(url)
+    #         try:
+    #             file = open(f'./{id}-output.json', 'r').read()
+    #             print(file)
+    #             load = json.loads(file)
+    #             try:
+    #                 d = load['done']['value']
+    #                 a = load['application']['value']
+    #                 if (d == 0):
+    #                     inf = {'info': f'{id}, done={d}'}
+    #                     print(inf)
+    #                     n = 6
+    #                     url = f'{uri}/resources/status/{id}?state={n}&application={a}'
+    #             except KeyError as ke:
+    #                 err = {'error': f'{id}, KeyError {ke}'}
+    #                 print(err)
+    #                 n = 8
+    #                 url = f'{uri}/resources/status/{id}?state={n}'
+    #         except FileNotFoundError as fe:
+    #             err = {'error': f'{id}, FileNotFoundError {fe}'}
+    #             print(err)
+    #             n = 10
+    #             url = f'{uri}/resources/status/{id}?state={n}'
+    #         response = requests.get(url)
+    #         if response.status_code != 200 and response.status_code != 201 and response.status_code != 202:
+    #             err = {'error': f'resource state, web {response.status_code}'}
+    #             raise Exception(err)
+    #         return 0
+
+    def status(self, id, code=0, app='null', link='null'):
             if link == 'null':
                 protocol = environ.get('IAC_ENDPOINT_PROTOCOL')
                 hostname = environ.get('IAC_ENDPOINT_HOSTNAME')
@@ -76,34 +117,15 @@ class HttpFetch(object):
                 uri = f'{protocol}://{hostname}:{port}'
             else:
                 uri = f'{link}'
-            url = f'{uri}/resources/query/{id}?document=tagging'
-            n = 0
-            d = 0
+            url = f'/resources/query/{id}?document=tagging'
             # debugging
             # print(url)
             try:
-                file = open(f'./{id}-output.json', 'r').read()
-                print(file)
-                load = json.loads(file)
-                try:
-                    d = load['done']['value']
-                    a = load['application']['value']
-                    if (d == 0):
-                        inf = {'info': f'{id}, done={d}'}
-                        print(inf)
-                        n = 6
-                        url = f'{protocol}://{hostname}:{port}/resources/status/{id}?state={n}&application={a}'
-                except KeyError as ke:
-                    err = {'error': f'{id}, KeyError {ke}'}
-                    print(err)
-                    n = 8
-                    url = f'{protocol}://{hostname}:{port}/resources/status/{id}?state={n}'
-            except FileNotFoundError as fe:
-                err = {'error': f'{id}, FileNotFoundError {fe}'}
-                print(err)
-                n = 10
-                url = f'{protocol}://{hostname}:{port}/resources/status/{id}?state={n}'
-            response = requests.get(url)
+                if app == 'state':
+                    url = f'{uri}/resources/status/{id}?state={code}&application={app}'
+                else:
+                    url = f'{uri}/resources/status/{id}?state={code}'
+                response = requests.get(url)
             if response.status_code != 200 and response.status_code != 201 and response.status_code != 202:
                 err = {'error': f'resource state, web {response.status_code}'}
                 raise Exception(err)
@@ -189,7 +211,6 @@ class HttpFetch(object):
         else:
             return 1
  
-
 
 if __name__ == '__main__':
     fire.Fire(HttpFetch)
