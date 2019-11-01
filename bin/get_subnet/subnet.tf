e801a502dcaaf865af3b16365eb669bf976a7bfa
molupini <info@myloft.co.za>
2019-11-01 12:22:12 +0200

    updated vpc variables to add perimeter external and variables

--- bin/get_subnet/subnet.tf
+++ bin/get_subnet/subnet.tf
@@ -7,17 +7,17 @@ data "aws_vpc" "vpc" {
 
 data "aws_subnet_ids" "subnet" {
   vpc_id = "${data.aws_vpc.vpc.id}"
-  filter {
-    name = "tag:Name"
-    values = [
-      # var.perimeter != "private" || var.perimeter != "public"
-      # "${var.perimeter == "default" ? "*" : "*-${lower(data.external.perimeter.result[var.perimeter])}-*"}",
-      # "${var.perimeter == "default" ? "*" : "*-${upper(data.external.perimeter.result[var.perimeter])}-*"}"
-      # "${var.perimeter == "default" ? data.external.perimeter.result[var.perimeter] : "*-${lower(data.external.perimeter.result[var.perimeter])}-*"}",
-      # "${var.perimeter == "default" ? data.external.perimeter.result[var.perimeter] : "*-${upper(data.external.perimeter.result[var.perimeter])}-*"}"
-      "*"
-    ]
-  }
+  # filter {
+  #   name = "tag:Name"
+  #   values = [
+  #     # var.perimeter != "private" || var.perimeter != "public"
+  #     # "${var.perimeter == "default" ? "*" : "*-${lower(data.external.perimeter.result[var.perimeter])}-*"}",
+  #     # "${var.perimeter == "default" ? "*" : "*-${upper(data.external.perimeter.result[var.perimeter])}-*"}"
+  #     # "${var.perimeter == "default" ? data.external.perimeter.result[var.perimeter] : "*-${lower(data.external.perimeter.result[var.perimeter])}-*"}",
+  #     # "${var.perimeter == "default" ? data.external.perimeter.result[var.perimeter] : "*-${upper(data.external.perimeter.result[var.perimeter])}-*"}"
+  #     "*"
+  #   ]
+  # }
 }
 
 data "aws_subnet" "subnet" {
