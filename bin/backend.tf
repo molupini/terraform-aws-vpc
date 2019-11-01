8f87df210f7e7182ad7e94222c64caa3275d218a
molupini <info@myloft.co.za>
2019-11-01 11:04:47 +0200

    updated tf backend

--- bin/backend.tf
+++ bin/backend.tf
@@ -4,8 +4,8 @@
 terraform {
   backend "s3" {
     key    = "automation.state"
-    bucket = "s3-myloft-sbx-000002"
-    dynamodb_table = "dyn-myloft-sbx-000002"
+    # bucket = "s3-myloft-sbx-000002"
+    # dynamodb_table = "dyn-myloft-sbx-000002"
     # region = "eu-west-1"
     # encrypt = "true"
   }
